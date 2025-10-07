2eea02968ea20a1a09a3f2fe09c74f0434d235d1
Ostap Komar <okomar@Ostaps-MacBook-Pro-8178.local>
2025-10-07 16:16:10 +0200

    Assignment 1.1

--- src/main/java/study/spring/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/study/spring/spring6webapp/bootstrap/BootstrapData.java
@@ -1,4 +1,72 @@
 package study.spring.spring6webapp.bootstrap;
 
-public class BootstrapData {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import study.spring.spring6webapp.domain.Author;
+import study.spring.spring6webapp.domain.Book;
+import study.spring.spring6webapp.domain.Publisher;
+import study.spring.spring6webapp.repositories.AuthorRepository;
+import study.spring.spring6webapp.repositories.BookRepository;
+import study.spring.spring6webapp.repositories.PublisherRepository;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository,  PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Design");
+        ddd.setIsbn("123456");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+        Author rod = new Author();
+        eric.setFirstName("Rod");
+        eric.setLastName("Johnson");
+
+        Book noEJB = new Book();
+        ddd.setTitle("J2EE Development without EJB");
+        ddd.setIsbn("54757585");
+
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        ericSaved.getBooks().add(dddSaved);
+        rodSaved.getBooks().add(noEJBSaved);
+
+        authorRepository.save(ericSaved);
+        authorRepository.save(rodSaved);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+
+        Publisher james = new Publisher();
+        james.setAddress("BakerStreet 221b");
+        james.setCity("London");
+        james.setPublisherName("Sherlock Times");
+        james.setState("Great Britain");
+        james.setZipCode("31-432");
+
+        Publisher jamesSaved = publisherRepository.save(james);
+
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+    }
 }
--- src/main/java/study/spring/spring6webapp/repositories/PublisherRepository.java
+++ src/main/java/study/spring/spring6webapp/repositories/PublisherRepository.java
@@ -1,4 +1,9 @@
 package study.spring.spring6webapp.repositories;
 
-public interface PublisherRepository {
+import org.springframework.data.repository.CrudRepository;
+import study.spring.spring6webapp.domain.Publisher;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+
+
 }
